bef3aa7c57103e7f843d86c6842d78a3fa551edd
otterSp0tter <hep.crossing@gmail.com>
2020-12-17 09:27:54 +1100

    initialized repository

---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  Widget build(context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          emailField(),
+          passwordField(),
+        ],
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return TextField();
+  }
+}
