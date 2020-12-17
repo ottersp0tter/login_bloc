1c507064591807ebdd86234774d7f6a029622912
Hephzibah Crossing <hep.crossing@gmail.com>
2020-12-17 16:36:00 +1100

    bloc and provider streams completed

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -1,22 +1,63 @@
 import 'package:flutter/material.dart';
+import '../blocs/bloc.dart';
+import '../blocs/provider.dart';
 
 class LoginScreen extends StatelessWidget {
   Widget build(context) {
+    final bloc = Provider.of(context);
     return Container(
       margin: EdgeInsets.all(20.0),
       child: Column(
         children: [
-          emailField(),
-          passwordField(),
-          FlatButton(
-            onPressed: () {},
+          emailField(bloc),
+          passwordField(bloc),
+          Container(
+            margin: EdgeInsets.only(top: 25.0),
           ),
+          submitButton(),
         ],
       ),
     );
   }
 
-  Widget emailField() {
-    return TextField();
+  Widget emailField(Bloc bloc) {
+    return StreamBuilder(
+        stream: bloc.email,
+        builder: (context, snapshot) {
+          return TextField(
+            onChanged: bloc.changeEmail,
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+              hintText: 'you@example.com',
+              labelText: 'Email Adress',
+              errorText: snapshot.error,
+            ),
+          );
+        });
+  }
+
+  Widget passwordField(Bloc bloc) {
+    return StreamBuilder(
+      stream: bloc.password,
+      builder: (context, snapshot) {
+        return TextField(
+          onChanged: bloc.changePassword,
+          obscureText: true,
+          decoration: InputDecoration(
+            hintText: 'Password',
+            labelText: 'Password',
+            errorText: snapshot.error,
+          ),
+        );
+      },
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      onPressed: () {},
+      child: Text('Login'),
+      color: Colors.pinkAccent,
+    );
   }
 }
